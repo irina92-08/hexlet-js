cb656136568cb41bce2e47239e5078e8eba5de6d
Irina Bugrova <irina92-08@mail.ru>
2024-03-04 16:01:18 +0300

    add src/index.js, index.js, main

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import _ from "lodash";
-
-console.log("Hello, Hexlet!");
-console.log(_.last(["one", "two"]));
+import { one, two } from './src/index.js';
+one();
+two();
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import _ from 'lodash';
+export const one = () => console.log("Hello, Hexlet!");
+export const two = () => console.log(_.last(["one", "two"]));
+
